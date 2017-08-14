6c716b1aaff87155c777f6d49fdab1596f45ad78
Tristan Schmitt <trschmitt50@live.com>
2017-08-14 12:00:59 -0400

    Models and migrations created.

---
+++ vending-machine-starter/server/migrations/20170814155849-create-data.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('data', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      cost: {
+        type: Sequelize.INTEGER
+      },
+      quantitiy: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('data');
+  }
+};
