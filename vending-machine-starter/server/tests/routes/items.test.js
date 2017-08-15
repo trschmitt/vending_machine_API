86648d4d2e22b88c70b16530707d42424aa47f68
Tristan Schmitt <trschmitt50@live.com>
2017-08-15 11:59:52 -0400

    Edited Testing spacing

--- vending-machine-starter/server/tests/routes/items.test.js
+++ vending-machine-starter/server/tests/routes/items.test.js
@@ -1,30 +1,36 @@
 const app = require("../../index");
 const request = require("supertest");
-const db = require('../../models');
+const db = require("../../models");
 const Item = db.item;
 
-describe('Item router', () => {
-  describe('GET /api/customer/items - get list of items', () => {
-    it('has a successful status code', () => {
-      return request(app)
-        .get("/api/customer/items")
-        .expect(200);
+describe("Item router", () => {
+  describe("GET /api/customer/items - get list of items", () => {
+
+    afterEach(() => {
+      return Item.destroy({ where: {} });
     });
-    it('has a status key in json body', () => {
-      return request(app)
-        .get("/api/customer/items")
-        .then((res) =>{
-          expect(res.body.status).toEqual("success");
-        })
+
+    it("has a successful status code", () => {
+      return request(app).get("/api/customer/items").expect(200);
     });
-    it('has items from database', () => {
-      return Item.create({ description:"Skittles", cost:75, quantity:20 }).then((item) => {
-        return request(app)
-          .get("/api/customer/items")
-          .then((res) => {
-            expect(res.body.data[0].description).toEqual("Skittles");
-          })
-      })
+
+    it("has a status key in json body", () => {
+      return request(app).get("/api/customer/items").then(res => {
+        expect(res.body.status).toEqual("success");
+      });
     });
+
+    it("has items from database", () => {
+      return Item.create({
+        description: "Skittles",
+        cost: 75,
+        quantity: 20
+      }).then(item => {
+        return request(app).get("/api/customer/items").then(res => {
+          expect(res.body.data[0].description).toEqual("Skittles");
+        });
+      });
+    });
+    
   });
 });
