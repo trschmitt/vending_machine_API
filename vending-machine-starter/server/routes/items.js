e89a7abaf4532683e08138b9096a5bc0b40fa7d3
Tristan Schmitt <trschmitt50@live.com>
2017-08-16 12:52:46 -0400

    Commit Message
    This project is not working perfectly but is not far off.
    Conceptually this is how an API is supposed to be created when using TDD
    pushing to Github to use for future reference.

--- vending-machine-starter/server/routes/items.js
+++ vending-machine-starter/server/routes/items.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 const Item = db.item;
+const Customer = db.customer;
+
+function currentCustomer(callback) {
+  Customer.findOne().then(callback);
+}
 
 router.get("/api/customer/items", (req, res) => {
-  Item.findAll().then((items) => {
+  Item.findAll().then(items => {
     res.json({
       status: "success",
       data: items
     });
-  })
-})
-
-router.post("/api/customer/items/:itemId/purchases", (req, res) => {
-  Item.findById({ where: {id: req.params.itemId} }).then((item) => {
-    res.json({
-      status: "success",
-      data: item
-    });
-  }).catch(err => {
-    throw(err); 
   });
 });
 
-module.exports = router
+router.post("/api/customer/items/:itemId/purchases", (req, res) => {
+  currentCustomer(customer => {
+    Item.findById(req.params.itemId).then(item => {
+      let result;
+      if (customer.money >= item.cost && item.quantity > 0) {
+        item.quantity -= 1;
+        customer.availableMoney -= item.cost;
+        item.save().then(() => {
+          customer.save().then(() => {
+            res.json({ status: "success" });
+          });
+        });
+      } else {
+        res.json({ status: "fail" });
+      }
+      res.json(result);
+    });
+  });
+});
+
+module.exports = router;
--- vending-machine-starter/server/tests/routes/items.test.js
+++ vending-machine-starter/server/tests/routes/items.test.js
@@ -5,11 +5,9 @@ const Item = db.item;
 const Customer = db.customer;
 
 describe("Item router", () => {
-
   describe("GET /api/customer/items - get list of items", () => {
-
     afterEach(() => {
-      return Item.destroy({where: {}});
+      return Item.destroy({ where: {} });
     });
 
     it("has a successful status code", () => {
@@ -36,34 +34,31 @@ describe("Item router", () => {
   });
 
   describe("POST /api/customer/items/:itemId/purchases - Purchase an item", () => {
+    afterEach(() => {
+      return Item.destroy({ where: {} });
+    });
 
     afterEach(() => {
-      return Item.destroy({where: {}});
+      return Customer.destroy({ where: {} });
     });
 
     it("has a successful status code", () => {
       return Item.create({
-          description: "Skittles",
-          cost: 75,
-          quantity: 20
-        }).then(item => {
-          request(app).post("/api/customer/items/:itemId/purchases").expect(200);
+        description: "Skittles",
+        cost: 75,
+        quantity: 20
+      }).then(item => {
+        Customer.create({
+          availableMoney: 100
+        }).then(customer => {
+          request(app)
+            .post("/api/customer/items/:itemId/purchases")
+            .expect(201);
+        });
       });
     });
 
-    it("has a status key in json body", () => {
-      return Item.create({
-          description: "GumDrops",
-          cost: 100,
-          quantity: 20
-        }).then(item => {
-           request(app).post("/api/customer/items/:itemId/purchases").then(res => {
-            expect(res.body.status).toEqual("success");
-          });
-      });
-    });
-
-    it("has an item from database", () => {
+    it("responds with status: success if customer has enough money", () => {
       return Item.create({
         description: "Oreos",
         cost: 99,
@@ -74,10 +69,86 @@ describe("Item router", () => {
         }).then(customer => {
           request(app)
             .post(`/api/customer/items/${item.id}/purchases`, {})
-            .then(res.body.status).toBe("success")
-        })
+            .then(res.body.status)
+            .toBe("success");
+        });
       });
     });
 
+    it("responds with status fail if customer doesnt have enough", () => {
+      return Item.create({
+        description: "Oreos",
+        cost: 99,
+        quantity: 20
+      }).then(item => {
+        Customer.create({
+          availableMoney: 90
+        }).then(customer => {
+          request(app)
+            .post(`/api/customer/items/${item.id}/purchases`, {})
+            .then(res.body.status)
+            .toBe("fail");
+        });
+      });
+    });
+
+    it("responds with status fail if Item Quantity is 0", () => {
+      return Item.create({
+        description: "Oreos",
+        cost: 99,
+        quantity: 0
+      }).then(item => {
+        Customer.create({
+          availableMoney: 100
+        }).then(customer => {
+          request(app)
+            .post(`/api/customer/items/${item.id}/purchases`, {})
+            .then(res.body.status)
+            .toBe("fail");
+        });
+      });
+    });
+
+    it("Reduces the item quantity by 1", () => {
+      return Item.create({
+        description: "Oreos",
+        cost: 99,
+        quantity: 2
+      }).then(item => {
+        Customer.create({
+          availableMoney: 100
+        }).then(customer => {
+          request(app)
+            .post(`/api/customer/items/${item.id}/purchases`, {})
+            .then(res => {
+              Item.findById(item.id).then(updatedItem => {
+                expect(updatedItem.quantity).toBe(item.quantity - 1);
+              });
+            });
+        });
+      });
+    });
+
+    it("Reduces the customer money by item cost", () => {
+      return Item.create({
+        description: "Oreos",
+        cost: 99,
+        quantity: 2
+      }).then(item => {
+        Customer.create({
+          availableMoney: 100
+        }).then(customer => {
+          request(app)
+            .post(`/api/customer/items/${item.id}/purchases`, {})
+            .then(res => {
+              Customer.findById(customer.id).then(updatedCustomer => {
+                expect(updatedCustomer.availableMoney).toBe(
+                  customer.availableMoney - item.cost
+                );
+              });
+            });
+        });
+      });
+    });
   });
 });
